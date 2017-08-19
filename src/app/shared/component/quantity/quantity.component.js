d35b584b422af9358d31c3952f8c093ff75c3a5f
seedy.dupuis <seedy.dupuis@gmail.com>
2017-08-20 00:03:51 +1000

    feat : edited quantity component with new component. impacted detail component

--- src/app/shared/component/quantity/quantity.component.js
+++ src/app/shared/component/quantity/quantity.component.js
@@ -1,21 +1,44 @@
 require('./quantity.component.scss');
 
 class QuantityController {
-  constructor() {
+  constructor($rootScope) {
+    this.rootScope = $rootScope;
+  }
+
+  $onInit(){
+    let self = this;
+    this.rootScope.$on('addcart.add', function(event, args){
+      if(self.confirm && args.id === self.id){
+        self.amount = 0;
+      }
+    });
 
   }
 
   add(){
+    if(!this.confirm){
+      this.rootScope.$emit('quantity.add',{id:this.id, amount:1});
+    }
     this.amount++;
   }
 
   remove(){
-    this.amount--;
+    if(this.amount > 0){
+      if(!this.confirm){
+        this.rootScope.$emit('quantity.remove',{id:this.id, amount:1});
+      }
+      this.amount--;
+    }
   }
+
 }
 
 const quantityComponent = {
-  bindings: { amount: '=' },
+  bindings: {
+    id: '<',
+    confirm: '<',
+    amount: '='
+  },
   controller: QuantityController,
   template: require('./quantity.component.html')
 };

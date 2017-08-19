6836d683316a69e63a8eefa1cc6794fe3f079d27
seedy.dupuis <seedy.dupuis@gmail.com>
2017-08-19 18:35:41 +1000

    feat : category component design + loading product images

--- src/app/core/category/category.component.js
+++ src/app/core/category/category.component.js
@@ -1,12 +1,17 @@
 require('./category.component.scss');
 
 import headerUrl from 'media/plates-header.jpg';
+import products from 'json/products.json';
 
 class CategoryController {
   constructor() {
     this.headerUrl = headerUrl;
+    this.products = products.map((product) => {
+      return angular.extend(product, {
+        src: require('media/' + product.image)
+      });
+    });
   }
-
 }
 
 const categoryComponent = {

0fb2a745b8a5da85963e6aa6288f78aa70a5daa3
seedy.dupuis <seedy.dupuis@gmail.com>
2017-08-20 09:51:59 +1000

    feat : storing id of each product added to the cart

--- src/app/shared/component/cartIcon/cartIcon.component.js
+++ src/app/shared/component/cartIcon/cartIcon.component.js
@@ -5,31 +5,77 @@ import CartDialogController from '../../../core/cartDialog/cartDialog.controller
 class CartIconController {
   constructor($rootScope, $mdDialog) {
     this.rootScope = $rootScope;
-    this.count = 0;
     this.dialogCtrl = CartDialogController;
     this.$mdDialog = $mdDialog;
+    this.events = [];
   }
 
   $onInit(){
+    if(!this.store){
+      this.store = {};
+      this.count = 0;
+    }
     this.watchQuantityEvents();
+  }
+
+  $onChanges(changes){
+    let storeChanges = changes.store;
+    if(storeChanges && storeChanges.currentValue !== undefined){
+      this.count = this.countFromStore();
+    }
+  }
+
+  $onDestroy(){
+    this.events.forEach((deregister) => deregister());
   }
 
   watchQuantityEvents(){
     let self = this;
-    this.rootScope.$on('quantity.add', function(event, args){
-      self.count+=args.amount;
-    });
-    this.rootScope.$on('addcart.add', function(event, args){
-      self.count+=args.amount;
-    });
-    this.rootScope.$on('quantity.remove', function(event, args){
-      self.count-=args.amount;
-    });
+
+    this.events.push(this.rootScope.$on('quantity.add', function(event, args){
+      self.storeElem(args.id, args.amount);
+    }));
+    this.events.push(this.rootScope.$on('addcart.add', function(event, args){
+      self.storeElem(args.id, args.amount);
+    }));
+    this.events.push(this.rootScope.$on('quantity.remove', function(event, args){
+      self.removeElem(args.id, args.amount);
+    }));
+  }
+
+  storeElem(id, amount){
+    if(this.store[id]){
+      this.store[id] += amount;
+    } else {
+      this.store[id] = amount;
+    }
+    this.count = this.countFromStore();
+  }
+
+  removeElem(id, amount){
+    if(this.store[id]){
+      if(this.store[id] > amount){
+        this.store[id] -= amount;
+      } else {
+        delete this.store[id];
+      }
+    }
+    this.count = this.countFromStore();
+  }
+
+  countFromStore(){
+    return Object.keys(this.store).reduce((aggr, key) => {
+      aggr+= this.store[key];
+      return aggr;
+    }, 0);
   }
 
   openCartDialog(event){
     this.$mdDialog.show({
       controller: this.dialogCtrl,
+      controllerAs: '$ctrl',
+      locals: {store: this.store},
+      bindToController: true,
       template: require('../../../core/cartDialog/cartDialog.html'),
       parent: angular.element(document.body),
       targetEvent: event,
@@ -43,6 +89,7 @@ class CartIconController {
 
 const cartIconComponent = {
   controller: CartIconController,
+  bindings: {store: '<'},
   template: require('./cartIcon.component.html')
 };
 

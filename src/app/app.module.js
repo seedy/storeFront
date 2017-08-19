1c7d9aad3d8b3c818ce3744e5931d32208a4c00f
seedy.dupuis <seedy.dupuis@gmail.com>
2017-08-19 14:48:24 +1000

    feat : material theming and modules registration

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,11 +1,25 @@
+// external modules
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
+import material from 'angular-material';
+
+// css
+import 'angular-material/angular-material.css';
+
+// material theme
+import theme from './app.material-theme';
+
+
+
+// modules
+import core from './core';
+import shop from './shop';
 
 // routing
 import routing from './app.routing';
 
-// modules
-import core from './core'
+const storeFront = angular.module('storefront', [uirouter, material, core, shop])
+  .config(routing)
+  .config(theme);
 
-angular.module('storefront', [uirouter, core])
-  .config(routing);
+export default storeFront;

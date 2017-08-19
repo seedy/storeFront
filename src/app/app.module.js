9c09d9f81aff61d0f62c9e6e45c2c06fbb2e7619
seedy.dupuis <seedy.dupuis@gmail.com>
2017-08-19 22:15:09 +1000

    feat : created shared module

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -13,12 +13,13 @@ import theme from './app.material-theme';
 
 // modules
 import core from './core';
+import shared from './shared';
 import shop from './shop';
 
 // routing
 import routing from './app.routing';
 
-const storeFront = angular.module('storefront', [uirouter, material, core, shop])
+const storeFront = angular.module('storefront', [uirouter, material, core, shared, shop])
   .config(routing)
   .config(theme);
 

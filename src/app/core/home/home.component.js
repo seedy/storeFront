bfed8292cae5caa8e10f66904d308e5cd3a88cab
seedy.dupuis <seedy.dupuis@gmail.com>
2017-08-20 16:11:06 +1000

    refactor : moved handling of rootscope watchers and store data in singleton home component

--- src/app/core/home/home.component.js
+++ src/app/core/home/home.component.js
@@ -2,10 +2,54 @@ require('./home.component.scss');
 
 import logoUrl from 'media/logo.png';
 
-
+// Home component is expected to be a singleton :
+// if you had two instances in the same time, rootScope event handling would be a mess
 class HomeController {
-  constructor() {
+  constructor($rootScope) {
+    this.rootScope = $rootScope;
     this.logoUrl = logoUrl;
+    this.store = {};
+    this.events = [];
+  }
+
+  $onInit(){
+    this.watchQuantityEvents();
+  }
+
+  $onDestroy(){
+    this.events.forEach((deregister) => deregister());
+  }
+
+  watchQuantityEvents(){
+    let self = this;
+
+    this.events.push(this.rootScope.$on('quantity.add', function(event, args){
+      self.storeElem(args.id, args.amount);
+    }));
+    this.events.push(this.rootScope.$on('addcart.add', function(event, args){
+      self.storeElem(args.id, args.amount);
+    }));
+    this.events.push(this.rootScope.$on('quantity.remove', function(event, args){
+      self.removeElem(args.id, args.amount);
+    }));
+  }
+
+  storeElem(id, amount){
+    if(this.store[id]){
+      this.store[id] += amount;
+    } else {
+      this.store[id] = amount;
+    }
+  }
+
+  removeElem(id, amount){
+    if(this.store[id]){
+      if(this.store[id] > amount){
+        this.store[id] -= amount;
+      } else {
+        delete this.store[id];
+      }
+    }
   }
 
 }
--- src/app/shared/component/cartIcon/cartIcon.component.js
+++ src/app/shared/component/cartIcon/cartIcon.component.js
@@ -1,22 +1,20 @@
 require('./cartIcon.component.scss');
 
+import angular from 'angular';
 import CartDialogController from '../../dialog/cartDialog/cartDialog.controller';
 
 class CartIconController {
-  constructor($rootScope, $mdDialog, $state) {
-    this.rootScope = $rootScope;
+  constructor($mdDialog, $state) {
     this.dialogCtrl = CartDialogController;
     this.$mdDialog = $mdDialog;
     this.state = $state;
-    this.events = [];
   }
 
   $onInit(){
-    if(!this.store){
+    if(!angular.isObject(this.store)){
       this.store = {};
       this.count = 0;
     }
-    this.watchQuantityEvents();
   }
 
   $onChanges(changes){
@@ -30,43 +28,7 @@ class CartIconController {
     this.count = this.countFromStore();
   }
 
-  $onDestroy(){
-    this.events.forEach((deregister) => deregister());
-  }
 
-  watchQuantityEvents(){
-    let self = this;
-
-    this.events.push(this.rootScope.$on('quantity.add', function(event, args){
-      self.storeElem(args.id, args.amount);
-    }));
-    this.events.push(this.rootScope.$on('addcart.add', function(event, args){
-      self.storeElem(args.id, args.amount);
-    }));
-    this.events.push(this.rootScope.$on('quantity.remove', function(event, args){
-      self.removeElem(args.id, args.amount);
-    }));
-  }
-
-  storeElem(id, amount){
-    if(this.store[id]){
-      this.store[id] += amount;
-    } else {
-      this.store[id] = amount;
-    }
-    this.count = this.countFromStore();
-  }
-
-  removeElem(id, amount){
-    if(this.store[id]){
-      if(this.store[id] > amount){
-        this.store[id] -= amount;
-      } else {
-        delete this.store[id];
-      }
-    }
-    this.count = this.countFromStore();
-  }
 
   countFromStore(){
     return Object.keys(this.store).reduce((aggr, key) => {
